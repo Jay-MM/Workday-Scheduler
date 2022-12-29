f39480b658dc13c24c02ef97277dfadd7003543b
Jesus M <marquez.jay444@gmail.com>
2022-12-29 16:58:29 -0600

    $ created color coding function and set up local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,38 @@
+
+const saveBtn = $(".saveBtn");
 // current Date is displayed at the top of the calendar
-let currentDay = document.getElementById('currentDay')
-currentDay.innerHTML= moment().format("dddd MMMM YYYY")
+let currentDay = document.getElementById('currentDay');
+currentDay.innerHTML= moment().format("dddd MMMM YYYY");
+
+// function checks the time and color codes the time blocks its respectivce colors
+function colorCode() {
+    const currentHour = moment().hours();
+
+$(".time-block").each(function() {
+    const timeBlock = parseInt($(this).attr('id'));
+    
+    if (currentHour === timeBlock){
+        $(this).addClass('present');
+    }else if (currentHour > timeBlock){
+        $(this).removeClass('present');
+        $(this).addClass('past');
+    }else if (currentHour < timeBlock){
+        $(this).removeClass('past');
+        $(this).addClass('future');
+
+    }
+})
+}
+
+// event gets stored in local storage
+saveBtn.on('click', function(e) {
+    e.preventDefault();
+    const hour = $(this).siblings('.hour').text()
+    const description = $(this).siblings('.description').val()
+
+    localStorage.setItem(hour, description)
+})
+
+// function calls
+
+colorCode();
